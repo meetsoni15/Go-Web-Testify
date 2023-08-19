1023ca95c07f78b7a7f2437784e5c296aa729ac2
Meet Soni <meet.soni1513@gmail.com>
2023-08-19 10:55:22 +0530

    Home handler manual test added

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
-func Test_application_HandlerHome(t *testing.T) {
+// Handler tests
+func Test_application_handlers(t *testing.T) {
 	theTests := []struct {
 		name               string
 		url                string
@@ -17,14 +20,11 @@ func Test_application_HandlerHome(t *testing.T) {
 	}
 
 	// Init application struct
-	var app application
 	mux := app.routes() // routes
 
 	// create httptest server
 	ts := httptest.NewTLSServer(mux)
 	defer ts.Close()
-
-	pathToTemplate = "../../templates"
 
 	// range through tests
 	for _, e := range theTests {
@@ -38,4 +38,40 @@ func Test_application_HandlerHome(t *testing.T) {
 			t.Errorf("For %s, expected status %d, but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
 		}
 	}
+}
+
+func Test_application_HandlerHome(t *testing.T) {
+	// create req
+	req := httptest.NewRequest("GET", "/", nil)
+
+	// create recorder
+	rr := httptest.NewRecorder()
+
+	// call context and add it to request
+	req = addContextAndSessionToReq(req, app)
+
+	// assign test handler
+	testHandler := http.HandlerFunc(app.HandlerHome)
+
+	// call handler
+	testHandler.ServeHTTP(rr, req)
+
+	// check status code
+	if rr.Code != http.StatusOK {
+		t.Errorf("expected status 200, but got %d", rr.Code)
+	}
+
+	if !strings.Contains(rr.Body.String(), "<small>From Session") {
+		t.Error("Doesn't contain expected body")
+	}
+}
+
+func getContext(r *http.Request) context.Context {
+	return context.WithValue(r.Context(), contextUserKey, "unknown")
+}
+
+func addContextAndSessionToReq(r *http.Request, app application) *http.Request {
+	r = r.WithContext(getContext(r))
+	ctx, _ := app.Session.Load(r.Context(), r.Header.Get("X-Session"))
+	return r.WithContext(ctx)
 }
